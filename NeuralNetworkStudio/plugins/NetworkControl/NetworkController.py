33965218761cc655632edc0b5d8231734e7a4227
unknown <deepraval2905@gmail.com>
2020-05-13 14:14:44 +0530

    Completed Basic UI of network controller

--- NeuralNetworkStudio/plugins/NetworkControl/NetworkController.py
+++ NeuralNetworkStudio/plugins/NetworkControl/NetworkController.py
@@ -3,10 +3,11 @@ TODO
 # Add proper scaling, arrangement, styling
 # optimize code
 '''
-from PySide2.QtWidgets import QWidget, QPushButton, QFormLayout, QLabel, QVBoxLayout, QHBoxLayout,QAbstractItemView, QGridLayout,QListWidget, QComboBox, QStackedLayout, QLineEdit
-from PySide2.QtGui import QDoubleValidator
-from PySide2.QtCore import Qt
-class NetworkController(QWidget):
+from PySide2.QtWidgets import QWidget, QPushButton, QTabWidget ,QFormLayout, QLabel,QStyledItemDelegate, QVBoxLayout, QHBoxLayout,QAbstractItemView, QGridLayout,QListWidget, QComboBox, QStackedLayout, QLineEdit
+from PySide2.QtGui import QDoubleValidator, QStandardItem, QFontMetrics, QPalette 
+from PySide2.QtCore import Qt, QEvent 
+
+class NetworkController(QWidget):   
     def __init__(self):
         super().__init__() 
         self.main_layout = QGridLayout()
@@ -56,9 +57,9 @@ class NetworkController(QWidget):
             "CategoricalHinge", 
             "hinge function",
             "squared_hinge function",
-            "categorical_hinge function"])
-        #self.loss_stack = QStackedLayout()
-        self.metrics_selector = QListWidget()
+            "categorical_hinge function",
+            "CUSTOM_my1"])
+        self.metrics_selector = CheckableComboBox()
         self.metrics_selector.addItems([
             "Accuracy",
             "BinaryAccuracy", 
@@ -90,17 +91,23 @@ class NetworkController(QWidget):
             "FalseNegatives", 
             "PrecisionAtRecall", 
             "SensitivityAtSpecificity", 
-            "SpecificityAtSensitivity" 
+            "SpecificityAtSensitivity",
+            "CUSTOM_my1"
         ])
-        self.metrics_selector.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.optimizer_selector.currentIndexChanged[int].connect(lambda index: self.optimizer_stack.setCurrentIndex(index))
         tmp = QWidget()
         tmp.setLayout(self.optimizer_stack)
+        self.run_eagerly = QComboBox()
+        self.run_eagerly.addItems(["True", "False"])
         self.main_layout.addWidget(QLabel("Loss Function:"), 0, 0)
         self.main_layout.addWidget(self.loss_selector, 0, 1)   
-        self.main_layout.addWidget(QLabel("Optimizer:"), 1, 0)
-        self.main_layout.addWidget(self.optimizer_selector, 1 , 1)
-        self.main_layout.addWidget(tmp,2, 0 , 1, 2)
+        self.main_layout.addWidget(QLabel("Metrics:"),1, 0)
+        self.main_layout.addWidget(self.metrics_selector, 1, 1)
+        self.main_layout.addWidget(QLabel("run_eagerly:"), 2, 0)
+        self.main_layout.addWidget(self.run_eagerly, 2, 1)
+        self.main_layout.addWidget(QLabel("Optimizer:"), 3, 0)
+        self.main_layout.addWidget(self.optimizer_selector, 3 , 1)
+        self.main_layout.addWidget(tmp,4, 0 , 1, 2)
         self.setLayout(self.main_layout)
         self.set_styling()
         
@@ -316,3 +323,121 @@ class FtrlUI(QWidget):
 
     def get_optimizer(self):
         return None
+
+#Thanks to stackoverflow
+class CheckableComboBox(QComboBox):
+
+    # Subclass Delegate to increase item height
+    class Delegate(QStyledItemDelegate):
+        def sizeHint(self, option, index):
+            size = super().sizeHint(option, index)
+            size.setHeight(20)
+            return size
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Make the combo editable to set a custom text, but readonly
+        self.setEditable(True)
+        self.lineEdit().setReadOnly(True)
+        # Make the lineedit the same color as QPushButton
+        palette = qApp.palette()
+        palette.setBrush(QPalette.Base, palette.button())
+        self.lineEdit().setPalette(palette)
+
+        # Use custom delegate
+        self.setItemDelegate(CheckableComboBox.Delegate())
+
+        # Update the text when an item is toggled
+        self.model().dataChanged.connect(self.updateText)
+
+        # Hide and show popup when clicking the line edit
+        self.lineEdit().installEventFilter(self)
+        self.closeOnLineEditClick = False
+
+        # Prevent popup from closing when clicking on an item
+        self.view().viewport().installEventFilter(self)
+
+    def resizeEvent(self, event):
+        # Recompute text to elide as needed
+        self.updateText()
+        super().resizeEvent(event)
+
+    def eventFilter(self, object, event):
+
+        if object == self.lineEdit():
+            if event.type() == QEvent.MouseButtonRelease:
+                if self.closeOnLineEditClick:
+                    self.hidePopup()
+                else:
+                    self.showPopup()
+                return True
+            return False
+
+        if object == self.view().viewport():
+            if event.type() == QEvent.MouseButtonRelease:
+                index = self.view().indexAt(event.pos())
+                item = self.model().item(index.row())
+
+                if item.checkState() == Qt.Checked:
+                    item.setCheckState(Qt.Unchecked)
+                else:
+                    item.setCheckState(Qt.Checked)
+                return True
+        return False
+
+    def showPopup(self):
+        super().showPopup()
+        # When the popup is displayed, a click on the lineedit should close it
+        self.closeOnLineEditClick = True
+
+    def hidePopup(self):
+        super().hidePopup()
+        # Used to prevent immediate reopening when clicking on the lineEdit
+        self.startTimer(100)
+        # Refresh the display text when closing
+        self.updateText()
+
+    def timerEvent(self, event):
+        # After timeout, kill timer, and reenable click on line edit
+        self.killTimer(event.timerId())
+        self.closeOnLineEditClick = False
+
+    def updateText(self):
+        texts = []
+        for i in range(self.model().rowCount()):
+            if self.model().item(i).checkState() == Qt.Checked:
+                texts.append(self.model().item(i).text())
+        text = ", ".join(texts)
+
+        # Compute elided text (with "...")
+        metrics = QFontMetrics(self.lineEdit().font())
+        elidedText = metrics.elidedText(text, Qt.ElideRight, self.lineEdit().width())
+        self.lineEdit().setText(elidedText)
+
+    def addItem(self, text, data=None):
+        item = QStandardItem()
+        item.setText(text)
+        if data is None:
+            item.setData(text)
+        else:
+            item.setData(data)
+        item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsUserCheckable)
+        item.setData(Qt.Unchecked, Qt.CheckStateRole)
+        self.model().appendRow(item)
+
+    def addItems(self, texts, datalist=None):
+        for i, text in enumerate(texts):
+            try:
+                data = datalist[i]
+            except (TypeError, IndexError):
+                data = None
+            self.addItem(text, data)
+
+    def currentData(self):
+        # Return the list of selected items data
+        res = "Selected Metrics : "
+        for i in range(self.model().rowCount()):
+            if self.model().item(i).checkState() == Qt.Checked:
+                res += str(self.model().item(i).data())
+        return res
