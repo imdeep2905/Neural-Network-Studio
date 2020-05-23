2b3bd0a710b00341eb7c381b55f473903a97940f
unknown <deepraval2905@gmail.com>
2020-05-23 19:32:32 +0530

    Removed Constructor arguments and made them static

--- NeuralNetworkStudio/plugins/Layerselectors/LayerSelector.py
+++ NeuralNetworkStudio/plugins/Layerselectors/LayerSelector.py
@@ -3,17 +3,21 @@ TODO
 # Set appropiate Styling
 # Fix paths
 '''
+import os
 import sys 
 from pathlib import Path
 from PySide2.QtWidgets import QPushButton, QVBoxLayout, QApplication ,QListWidget, QListView, QLabel, QWidget, QAbstractItemView, QSplitter 
 from PySide2.QtGui import QStandardItemModel, QStandardItem, QIcon, Qt
 from PySide2 import QtCore
 
+PATH = os.path.join('NeuralNetworkStudio','img_src')
+print('LayerSelector Loaded with path:' , PATH)
+
 class LayersList(QWidget):
     '''
     LayerList class which acts as collapsable list.
     '''
-    def __init__(self, name, layers):
+    def __init__(self, name, layers, expand = True):
         super().__init__()
         self.currently_expanded = True
         self.main_layout = QVBoxLayout()
@@ -22,11 +26,11 @@ class LayersList(QWidget):
         self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.expand_button = QPushButton(name)
         self.expand_button.setToolTip(f"List of {name} Layers")
-        self.expand_button.setIcon(QIcon(str(Path('NeuralNetworkStudio/img_src/LayersList_Up.png'))))
+        self.expand_button.setIcon(QIcon(os.path.join(PATH,'LayersList_Up.png')))
         self.layer_list = QListView()
         self.container_model = QStandardItemModel()
         for l in layers:
-            self.container_model.appendRow(QStandardItem(QIcon(str(Path('NeuralNetworkStudio/img_src/LayersList_Layer_Icon.png'))),l))
+            self.container_model.appendRow(QStandardItem(QIcon(os.path.join(PATH,'LayersList_Layer_Icon.png')),l))
         self.layer_list.setDragEnabled(True)
         self.layer_list.setModel(self.container_model)
         self.layer_list.setEditTriggers(QAbstractItemView.NoEditTriggers)
@@ -34,19 +38,20 @@ class LayersList(QWidget):
         self.main_layout.addWidget(self.layer_list, 0, Qt.AlignTop)
         self.expand_button.clicked.connect(self.expand)
         self.setLayout(self.main_layout)
-        self.resized_size = 19 * len(layers)
+        self.resized_size = 17 * len(layers)
         self.set_styling()
-        
+        if not expand:
+            self.expand()
         
     @QtCore.Slot()
     def expand(self):
         if self.currently_expanded:
             self.layer_list.setMaximumHeight(0)
-            self.expand_button.setIcon(QIcon(str(Path('NeuralNetworkStudio/img_src/LayersList_Down.png'))))
+            self.expand_button.setIcon(QIcon(os.path.join(PATH,'LayersList_Down.png')))
             self.currently_expanded = False
         else:
             self.layer_list.setMaximumHeight(self.resized_size)
-            self.expand_button.setIcon(QIcon(str(Path('NeuralNetworkStudio/img_src/LayersList_Up.png'))))
+            self.expand_button.setIcon(QIcon(os.path.join(PATH,'LayersList_Up.png')))
             self.currently_expanded = True
             
     def set_styling(self):
@@ -58,17 +63,41 @@ class LayersSelectorWidget(QWidget):
     '''
     LayerChoiceWidget class provide widget plugin for picking layers.
     '''
-    def __init__(self, names, layers):
+    def __init__(self):
         super().__init__()
+        names = ['Core Layers',
+                 'Convolution Layers',
+                 'Pooling Layers',
+                 'Recurrent Layers',
+                 'Preprocessing Layers',
+                 'Attention Layers',
+                 'Reshaping Layers',
+                 'Locally-Connected Layers'
+                 ]
+        layers = [
+            ['Dense', 'Activattion', 'Embedding', 'Masking', 'Lambda'],
+            ['Conv1D', 'Conv2D', 'Conv3D', 'SeparableConv1D', 'SeparableConv2D', 'DepthwiseConv2D', 'Conv2DTranspose', 'Conv3Dtranspose'],
+            ['MaxPooling1D', 'MaxPooling2D', 'MaxPooling3D', 'AveragePooling1D', 'AveragePooling2D', 'AveragePooling3D', 'GlobalMAxPooling1D', 'GlobalMAxPooling2D', 'GlobalMAxPooling3D', 'GlobalAveragePooling1D', 'GlobalAveragePooling2D', 'GlobalAveragePooling3D'],
+            ['LSTM', 'GRU', 'SimpleRNN', 'TimeDistributed', 'BiDirectional', 'ConvLSTM2D'],
+            ['TextToVector', 'Normalization'],
+            ['Attention', 'AdditiveAttention'],
+            ['Reshape', 'Flatten', 'Cropping1D', 'Cropping2D', 'Cropping3D', 'UpSampling1D', 'UpSampling2D', 'UpSampling3D', 'ZeroPadding1D', 'ZeroPadding2D', 'ZeroPadding3D'],
+            ['LocallyConnected1D', 'LocallyConnected2D']
+        ]
         self.main_layout = QVBoxLayout()
         for i in range(len(names)):
-            self.main_layout.addWidget(LayersList(names[i], layers[i]))
+            if i > 3:
+                self.main_layout.addWidget(LayersList(names[i], layers[i], False))
+            else:
+                self.main_layout.addWidget(LayersList(names[i], layers[i]))                
         self.main_layout.setSpacing(0)
         self.main_layout.setAlignment(Qt.AlignTop)        
         self.main_layout.setMargin(0)
         self.main_layout.setSpacing(0)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.main_layout)
+        del(names)
+        del(layers)
         self.set_styling()
         
     def set_styling(self):
--- NeuralNetworkStudio/plugins/Toolbar/ToolBar.py
+++ NeuralNetworkStudio/plugins/Toolbar/ToolBar.py
@@ -5,18 +5,48 @@ TODO
 '''
 from PySide2.QtWidgets import QMainWindow, QApplication, QAction, QToolBar, QWidget
 import sys
+import os
 from PySide2.QtGui import QIcon, QFont
 
+
+PATH = os.path.join('NeuralNetworkStudio','img_src')
+print('ToolBar Loaded with path:' , PATH)
 
 class ToolBarWidget(QToolBar):
     '''
     ToolBar class Provides toolbar for window.
     '''
-    def __init__(self, names, icons):
+    def __init__(self):
         super().__init__()
+        names = ['New Project',
+        'Save project', 
+        'Load Project', 
+        'Custom', 
+        'Start' ,
+        'Reset',
+        'Launch Data Preprocessor',
+        'Fullscreen Mode',
+        'Preferences',
+        'Python Path',
+        'Performance',
+        'Help']
+        icons = [os.path.join(PATH,'ToolBar_New'),
+                 os.path.join(PATH,'ToolBar_Save'),
+                 os.path.join(PATH,'ToolBar_Load'),
+                 os.path.join(PATH,'ToolBar_Custom'),
+                 os.path.join(PATH,'ToolBar_Start'),
+                 os.path.join(PATH,'ToolBar_Reset'),
+                 os.path.join(PATH,'ToolBar_Data'),
+                 os.path.join(PATH,'ToolBar_Fullscreen'),
+                 os.path.join(PATH,'ToolBar_Preferences'),
+                 os.path.join(PATH,'ToolBar_Python'),
+                 os.path.join(PATH,'ToolBar_Performance'),
+                 os.path.join(PATH,'ToolBar_Help')]
         self.setMovable(False)
         for i in range(len(names)):
             self.addAction(QAction(QIcon(icons[i]), names[i], self))
+        del(names)
+        del(icons)
         self.set_styling()
 
     def set_styling(self):
