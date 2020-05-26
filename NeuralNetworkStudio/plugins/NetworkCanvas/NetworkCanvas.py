d6083d298847a244ae0c9033a7ab0144a93034ab
unknown <deepraval2905@gmail.com>
2020-05-26 18:38:45 +0530

    Added Basic Support for drops

--- NeuralNetworkStudio/plugins/NetworkCanvas/NetworkCanvas.py
+++ NeuralNetworkStudio/plugins/NetworkCanvas/NetworkCanvas.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QWidget, QTableView
+from PySide2.QtWidgets import QWidget, QTableView, QVBoxLayout
 from PySide2 import QtCore
 from PySide2.QtGui import QStandardItem, QStandardItemModel
 from plugins.LayerBox.LayerBox import LayerBoxWidget
@@ -7,6 +7,9 @@ class NetworkCanvasWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.setAcceptDrops(True)
+        self.main_layout = QVBoxLayout()
+        self.setLayout(self.main_layout)
+        self.set_styling()
         
     def dragEnterEvent(self, event):
         if event.mimeData().hasFormat("application/x-qabstractitemmodeldatalist"):
@@ -17,6 +20,9 @@ class NetworkCanvasWidget(QWidget):
         tmp.dropMimeData(event.mimeData(), event.dropAction(), 0, 0, QtCore.QModelIndex())
         self.add_to_layout(LayerBoxWidget(tmp.index(0, 0).data()))
         
-    def add_to_layout(self, layer):
+    def set_styling(self):
         pass
+    
+    def add_to_layout(self, layer):
+        self.main_layout.addWidget(layer)
     
