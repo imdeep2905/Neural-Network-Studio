de51c5c34ae2c0a860f36b1bc532e7b3df8ad986
unknown <deepraval2905@gmail.com>
2020-05-26 18:38:28 +0530

    Done some major changes

--- NeuralNetworkStudio/plugins/Layerselectors/LayerSelector.py
+++ NeuralNetworkStudio/plugins/Layerselectors/LayerSelector.py
@@ -6,9 +6,10 @@ TODO
 import os
 import sys 
 from pathlib import Path
-from PySide2.QtWidgets import QPushButton, QVBoxLayout, QApplication ,QListWidget, QListView, QLabel, QWidget, QAbstractItemView, QSplitter 
+from PySide2.QtWidgets import QPushButton, QVBoxLayout, QApplication ,QListWidget,QScrollArea, QListView, QLabel, QWidget, QAbstractItemView, QSplitter 
 from PySide2.QtGui import QStandardItemModel, QStandardItem, QIcon, Qt, QDrag
 from PySide2 import QtCore
+from PySide2 import QtGui
 
 PATH = os.path.join('.','img_src')
 print('LayerSelector Loaded with path:' , PATH)
@@ -19,46 +20,68 @@ class LayersList(QWidget):
     '''
     def __init__(self, name, layers, expand = True):
         super().__init__()
+        self.setWindowModality(QtCore.Qt.WindowModal)
         self.currently_expanded = True
         self.main_layout = QVBoxLayout()
         self.main_layout.setMargin(0)
         self.main_layout.setSpacing(0)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
+        
         self.expand_button = QPushButton(name)
         self.expand_button.setToolTip(f"List of {name} Layers")
         self.expand_button.setIcon(QIcon(os.path.join(PATH,'LayersList_Up.png')))
+        
         self.layer_list = QListView()
         self.layer_list.setDragEnabled(True)
+        self.layer_list.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.layer_list.setWrapping(False)
+        self.layer_list.setViewMode(self.layer_list.ListMode)
+        self.layer_list.setResizeMode(self.layer_list.Fixed)
+        
         self.container_model = QStandardItemModel()
         for l in layers:
-            qs = CustomQStandardItem(QIcon(os.path.join(PATH,'LayersList_Layer_Icon.png')),l)
-            self.container_model.appendRow(qs)
+            self.container_model.appendRow(QStandardItem(QIcon(os.path.join(PATH,'LayersList_Layer_Icon.png')),l))
+        
         self.layer_list.setModel(self.container_model)
-        self.layer_list.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.layer_list.setMaximumSize(self.get_container_sizes())
+        self.layer_list.setMinimumSize(self.get_container_sizes())
+        
         self.main_layout.addWidget(self.expand_button,0, Qt.AlignTop)
         self.main_layout.addWidget(self.layer_list, 0, Qt.AlignTop)
         self.expand_button.clicked.connect(self.expand)
+        
         self.setLayout(self.main_layout)
-        self.resized_size = 16.5 * len(layers)
+        self.resized_size = self.layer_list.maximumHeight()
+        
         self.set_styling()
+        
         if not expand:
             self.expand()
             
     @QtCore.Slot()
     def expand(self):
         if self.currently_expanded:
-            self.layer_list.setMaximumHeight(0)
-            self.expand_button.setIcon(QIcon(os.path.join(PATH,'LayersList_Down.png')))
             self.currently_expanded = False
+            self.expand_button.setIcon(QIcon(os.path.join(PATH,'LayersList_Down.png')))
+            self.layer_list.setMaximumHeight(0)
         else:
-            self.layer_list.setMaximumHeight(self.resized_size)
-            self.expand_button.setIcon(QIcon(os.path.join(PATH,'LayersList_Up.png')))
             self.currently_expanded = True
+            self.expand_button.setIcon(QIcon(os.path.join(PATH,'LayersList_Up.png')))
+            self.layer_list.setMinimumHeight(self.resized_size)
             
+    def get_container_sizes(self):
+        w, h = 0, 0
+        for i in range(self.container_model.columnCount()):
+            pass
+            #w += self.container_model.takeColumn(i) 
+        for i in range(self.container_model.rowCount()):
+            for r in self.container_model.takeRow(i):
+                h += r.sizeHint().height()
+        return QtCore.QSize(w, h)
+    
     def set_styling(self):
-        self.setStyleSheet("background-color:aliceblue ;")
-        self.expand_button.setStyleSheet("text-align:left;")
-        self.layer_list.setStyleSheet("background-color:aliceblue; border:1px solid;border-style: ridge;")
+        self.setStyleSheet("background-color:white;")
+        self.expand_button.setStyleSheet("background-color:lightgrey;text-align:left;")
 
 class LayersSelectorWidget(QWidget):
     '''
@@ -96,10 +119,23 @@ class LayersSelectorWidget(QWidget):
         self.main_layout.setMargin(0)
         self.main_layout.setSpacing(0)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.scroll_area = QScrollArea()
+        names = QWidget()
+        names.setLayout(self.main_layout)
+        self.scroll_area.setWidget(names)
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setAlignment(Qt.AlignTop)        
+        self.main_layout.setMargin(0)
+        self.main_layout.setSpacing(0)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.scroll_area.setAlignment(Qt.AlignTop)
+        self.scroll_area.setWidgetResizable(True)
+        self.main_layout.addWidget(self.scroll_area)
         self.setLayout(self.main_layout)
         del(names)
         del(layers)
+        
         self.set_styling()
         
     def set_styling(self):
-        self.setStyleSheet("background-color:aliceblue; border:0px solid;border-style: solid;")
+        self.setStyleSheet("background-color:aliceblue;")
